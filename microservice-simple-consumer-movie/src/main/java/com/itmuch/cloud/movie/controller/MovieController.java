78f4bb09233502b9311bacb1211b45157edf3ef6
lxy <acesdream@126.com>
2018-02-27 13:32:24 +0800

    chapter3：把应用中的变量定义在配置文件，并通过@Value注入

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/movie/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/movie/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.itmuch.cloud.movie.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,14 @@ public class MovieController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Value("${user.userServiceUrl}")
+	private String userServiceUrl;
+	
 	private Logger logger = LoggerFactory.getLogger(MovieController.class);
 	
 	@GetMapping("/user/{id}")
 	public User findById(@PathVariable Long id) {
-		User findOne = this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+		User findOne = this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
 		
 		logger.info("根据id:{}，查询到的user：{}", id, findOne);
 		return findOne;

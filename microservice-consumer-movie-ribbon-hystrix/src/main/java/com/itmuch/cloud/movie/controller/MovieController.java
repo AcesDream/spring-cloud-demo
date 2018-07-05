a30625e6f15fbcfa64ff8675cef0d1767531fb33
艾斯的梦想 <acesdream@126.com>
2018-07-05 16:43:56 +0800

    断路器hystrix的使用： 1、启动类上添加@EnableCircuitBreaker 或者  @EnableHystrix 2、controller的方法上使用注解@HystrixCommand(fallbackMethod = "findByIdFallback")

---
+++ microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/movie/controller/MovieController.java
@@ -0,0 +1,69 @@
+package com.itmuch.cloud.movie.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.movie.model.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+/**
+ * 
+ * 通过
+ * @HystrixCommand(fallbackMethod = "findByIdFallback")
+ * 
+ * 定义了findById方法的回退方法findByIdFallback，
+ * 
+ * @ClassName: MovieController
+ * @Description: TODO
+ *
+ */
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	private Logger logger = LoggerFactory.getLogger(MovieController.class);
+	
+	//produces：指定返回的格式
+	@HystrixCommand(fallbackMethod = "findByIdFallback")
+	@GetMapping(value= "/user/{id}", produces = "application/json")
+	public User findById(@PathVariable Long id) {
+		
+		/**
+		 * 注意：不能将restTemplate.getForObject(...)与loadBalancerClient.choose(...)写在同一个方法中
+		 * 两者之间会有冲突，因为此时restTemplate实际上是一个Ribbon客户端，本身已经包含了choose的行为
+		 */
+		//这里microservice-provider-user是虚拟主机名，虚拟主机名不能包含"_"之类的字符
+		User findOne = this.restTemplate.getForObject("http://microservice-provider-user/" + id, User.class);
+		
+		logger.info("根据id:{}，查询到的user：{}", id, findOne);
+		return findOne;
+	}
+	
+	@GetMapping("/log-user-instance")
+	public void logUserInstance() {
+		ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-user");
+		
+		logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+	}
+	
+	public User findByIdFallback(Long id) {
+		User user = new User();
+		user.setId(-1L);
+		user.setName("默认用户");
+		
+		return user;
+	}
+	
+}

da02983b3ddf13a1ddb929feece56323bef9fa78
艾斯的梦想 <acesdream@126.com>
2018-07-06 17:50:55 +0800

    turbine与rabbitmq结合

---
+++ microservice-consumer-movie-ribbon-hystrix-turbine-mq/src/main/java/com/itmuch/cloud/ConsumerMovieApplication.java
@@ -0,0 +1,37 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 断路器，需要在启动类上添加如下注解：
+ * @EnableCircuitBreaker 或者  @EnableHystrix
+ * 可以查看hystrix的监控信息
+ * http://localhost:8010/hystrix.stream
+ * 
+ * @ClassName: ConsumerMovieApplication
+ * @Description: TODO
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableCircuitBreaker
+public class ConsumerMovieApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMovieApplication.class, args);
+	}
+}

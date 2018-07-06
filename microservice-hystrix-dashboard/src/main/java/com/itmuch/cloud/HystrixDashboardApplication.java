4950e533828bb6516c3f9c3babd97827aa144e54
艾斯的梦想 <acesdream@126.com>
2018-07-06 14:54:02 +0800

    使用hystrix dashboard可视化监控数据， 只可以监控一个端点 @EnableHystrixDashboard http://localhost:8030/hystrix

---
+++ microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @EnableHystrixDashboard : 使用hystrix dashboard实现可视化监控
+ * 
+ * 它只能监控一个端点
+ * http://localhost:8030/hystrix
+ * @author lxy
+ *
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}

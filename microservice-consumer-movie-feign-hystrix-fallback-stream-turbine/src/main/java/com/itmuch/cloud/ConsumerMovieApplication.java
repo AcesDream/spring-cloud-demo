5064a85cc7883f9a50af61f71b7ee90e8653e72d
艾斯的梦想 <acesdream@126.com>
2018-07-06 16:42:12 +0800

    使用turbine聚合监控信息： 1、启动microservice-discovery-eureka 2、启动microservice-provider-user 3、启动microservice-consumer-movie-ribbon-hystrix 4、启动microservice-consumer-movie-feign-hystrix-fallback-stream-turbine 5、启动：microservice-hystrix-turbine 6、启动：microservice-hystrix-dashboard
    打开：http://localhost:8030/hystrix
    
    指定：
    http://localhost:8031/turbine.stream&title=聚合

---
+++ microservice-consumer-movie-feign-hystrix-fallback-stream-turbine/src/main/java/com/itmuch/cloud/ConsumerMovieApplication.java
@@ -0,0 +1,26 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
+public class ConsumerMovieApplication {
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMovieApplication.class, args);
+	}
+}
---
+++ microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/TurbineApplication.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @EnableTurbine : 可以聚合监控数据，同时监控多个微服务
+ * 
+ * 它只能监控一个端点
+ * http://localhost:8030/hystrix
+ * @author lxy
+ *
+ */
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+}

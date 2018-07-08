d6ac3a53aa1fa18cb1fae7eb3178833652c0982e
艾斯的梦想 <acesdream@126.com>
2018-07-08 15:50:43 +0800

    最简单网关zuul

---
+++ microservice-api-gateway/src/main/java/com/itmuch/cloud/ZuulApiGatewayApplication.java
@@ -0,0 +1,39 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 使用@EnableZuulProxy注解激活zuul
+ * 
+ * 跟进该注解可以看到该注解整合了@EnableCircuitBreaker、@EnableDiscoveryClient，是个组合注解，目的是简化配置
+ * 
+ * 这就是最简单的网关服务
+ * 
+ * 访问：
+ * http://GATEWAY:GATEWAY_PORT/想要访问的Eureka服务id的小写/**
+ * 
+ * 实际会转到：
+ * http://想要访问的Eureka服务id的小写:该服务端口/**
+ * 
+ * 1、启动microservice-discovery-eureka
+ * 2、启动microservice-provider-user
+ * 3、启动microservice-api-gateway
+ * 
+ * 访问：
+ * http://localhost:8050/microservice-provider-user/1
+ * 
+ * 实际上会转到：
+ * http://localhost:8001/1
+ * 
+ * @author lxy
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+}
